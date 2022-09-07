7e8972c6565b2726b2af5a4b16a35338540cffd1
Yunus Baki Karaça <yunusbaki38@gmail.com>
2022-09-07 17:51:32 +0300

    Add project files.

---
+++ src/project/Application/Features/ProgrammingLanguages/Commands/DeleteProgrammingLanguage/DeleteProgrammingLanguageCommand.cs
@@ -0,0 +1,50 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage
+{
+    public class DeleteProgrammingLanguageCommand : IRequest<DeletedProgrammingLanguageDto>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public class DeleteProgrammingLanguageCommandHandler : IRequestHandler<DeleteProgrammingLanguageCommand, DeletedProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+
+            public DeleteProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+                _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+            }
+
+            public async Task<DeletedProgrammingLanguageDto> Handle(DeleteProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                await _programmingLanguageBusinessRules.ProgrammingLanguageMustExist(request.Id);
+
+                //ProgrammingLanguage mappedProgrammingLanguage = _mapper.Map<ProgrammingLanguage>(request);
+                //ProgrammingLanguage deletedProgrammingLanguage = await _programmingLanguageRepository.DeleteAsync(mappedProgrammingLanguage);
+                //DeletedProgrammingLanguageDto result = _mapper.Map<DeletedProgrammingLanguageDto>(deletedProgrammingLanguage);
+
+                ProgrammingLanguage programmingLanguage = await _programmingLanguageRepository.GetAsync(p => p.Id == request.Id);
+                ProgrammingLanguage deletedProgrammingLanguage = await _programmingLanguageRepository.DeleteAsync(programmingLanguage);
+                DeletedProgrammingLanguageDto result = _mapper.Map<DeletedProgrammingLanguageDto>(deletedProgrammingLanguage);
+
+                return result;
+            }
+        }
+    }
+}
---
+++ src/project/Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.ProgrammingLanguages.Commands.DeleteProgrammingLanguage;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCanNotBeDuplicated(string name)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p => p.Name == name);
+            if (result.Items.Any()) throw new BusinessException("Programming Language name exists.");
+        }
+
+        public async Task ProgrammingLanguageMustExist(int id)
+        {
+            IPaginate<ProgrammingLanguage> result = await _programmingLanguageRepository.GetListAsync(p=>p.Id == id);
+            if (!result.Items.Any()) throw new BusinessException("Programming Language does not exist.");
+
+        }
+    }
+}
